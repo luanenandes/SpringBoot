5a69aa117a6ed25b6d52883b5e214c6b8ddd0a8a
Luanefernandes <65264021+Luanefernandes@users.noreply.github.com>
2020-07-30 08:36:00 -0300

    Crianção de API - CRUD -R01

--- minhaEscola/src/main/java/org/generation/minhaEscola/controller/TurmaController.java
+++ minhaEscola/src/main/java/org/generation/minhaEscola/controller/TurmaController.java
@@ -1,16 +1,22 @@
 package org.generation.minhaEscola.controller;
 import org.generation.minhaEscola.repository.TurmaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
 
+import org.generation.blogPessoal.model.Postagem;
 import org.generation.minhaEscola.model.Turma;
 
 
@@ -39,6 +45,19 @@ public class TurmaController {
 		return ResponseEntity.ok(repository.findAllByTurmaContainingIgnoreCase(turma));
 	}
 	
+	@PostMapping
+	public ResponseEntity<Turma> post(@RequestBody Turma turmas){
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(turmas));
+	}
 	
+	@PutMapping
+	public ResponseEntity<Turma> put(@RequestBody Turma turmas){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(turmas));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id){
+		repository.deleteById(id);
+	}
 	
 }
